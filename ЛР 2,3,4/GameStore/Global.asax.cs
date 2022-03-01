cf7f2d16739c84b8fdfa78d59271bb269a4b8dae
lHlope <72273721+lHlope@users.noreply.github.com>
2022-03-01 16:55:29 +0300

    Add files via upload

---
+++ ЛР 2,3,4/GameStore/Global.asax.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace GameStore
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
+

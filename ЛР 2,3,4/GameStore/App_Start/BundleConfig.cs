c1f29e9e1e09fc1a05b768649d73fd691bf94444
lHlope <72273721+lHlope@users.noreply.github.com>
2022-03-01 16:21:39 +0300

    Добавлена ЛР 2,3,4

---
+++ ЛР 2,3,4/GameStore/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace GameStore
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/validation").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js"));
+        }
+    }
+}
